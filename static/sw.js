6fce74fbb9287610beb8f5b4ea3e6a65d8f51281
flamingopink7 <livio.tillema@ost.ch>
2026-03-09 16:37:37 +0100

    Refactor service worker caching logic

--- static/sw.js
+++ static/sw.js
@@ -1,23 +1,20 @@
-const CACHE_NAME = 'wg-app-cache-v1';
-const urlsToCache = [
+const CACHE_NAME = 'wg-app-v1';
+const ASSETS = [
   '/',
-  '/app/static/manifest.json'
+  '/icon.png',
+  '/manifest.json'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
